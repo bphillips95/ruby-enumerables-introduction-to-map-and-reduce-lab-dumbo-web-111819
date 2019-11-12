d5c7892e7be48e7014572a32f77c4b43c16b0b4b
Baruch Phillips <baruchphillips@gmail.com>
2019-11-12 17:51:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ def reduce_to_total(source_array,starting_point=0)
 i = 0 
 array = []
 while i < source_array.length do 
- starting_point += source_array
+ starting_point += source_array[i]
   i+=1 
  end
   return starting_point
